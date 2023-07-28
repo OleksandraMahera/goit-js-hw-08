0f0d6f5552e33f5d748d79c27ee9bfec25030e9a
OleksandraMahera <126208886+OleksandraMahera@users.noreply.github.com>
2023-07-28 21:23:12 +0300

    js-hw-08

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,22 @@
 
+
+import Vimeo from '@vimeo/player';
+import throttle from 'lodash.throttle';
+
+const iframe = document.querySelector('iframe');
+const player = new Vimeo(iframe);
+const LOCAL_STORAGE_KEY = "videoplayer-current-time";
+const timeFromLocalStorage = localStorage.getItem(LOCAL_STORAGE_KEY) || 0;
+
+const onPlay = function(data) {
+    const currentTimeWatchingVideo = data.seconds;
+    localStorage.setItem(LOCAL_STORAGE_KEY, currentTimeWatchingVideo);
+};
+
+player.on('timeupdate', throttle(onPlay, 1000));
+
+player.setCurrentTime(timeFromLocalStorage);
+
+player.getVideoTitle().then(function(title) {
+    console.log('title:', title);
+});
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,48 @@
+import throttle from 'lodash.throttle';
+
+const form = document.querySelector('.feedback-form');
+const input = document.querySelector('input');
+const textarea = document.querySelector('textarea');
+
+const LOCAL_STORAGE_KEY = "feedback-form-state";
+const userFeedbackData = {
+    email: '',
+    message: '',
+};
+
+if (localStorage.getItem(LOCAL_STORAGE_KEY) !== null) {
+    input.value = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY)).email;
+    textarea.value = JSON.parse(
+    localStorage.getItem(LOCAL_STORAGE_KEY)
+    ).message;
+    userFeedbackData.email = input.value;
+    userFeedbackData.message = textarea.value;
+};
+
+
+form.addEventListener('input', throttle(onSavingFeedback, 500));
+form.addEventListener('submit', onResetForm);
+
+function onSavingFeedback(evt) {
+    userFeedbackData[evt.target.name] = evt.target.value;
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(userFeedbackData));
+};
+
+function onResetForm(evt) {
+    evt.preventDefault();
+
+    const {
+        elements: { email, message }
+    } = evt.currentTarget;
+    
+    if (email.value === "" || message.value === "") {
+        return alert("Please fill in all the fields!");
+    } else {
+        localStorage.removeItem(LOCAL_STORAGE_KEY);
+        evt.currentTarget.reset();
+        console.log(userFeedbackData);
+        userFeedbackData.email = '';
+        userFeedbackData.message = '';
+    }
+}
 
