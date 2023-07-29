87dad6b7a29a42ebefe4e9e2bc18f5fb77306e77
OleksandraMahera <126208886+OleksandraMahera@users.noreply.github.com>
2023-07-29 18:38:03 +0300

    js-hw-08

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,12 +1,12 @@
-
-
 import Vimeo from '@vimeo/player';
 import throttle from 'lodash.throttle';
 
 const iframe = document.querySelector('iframe');
 const player = new Vimeo(iframe);
 const LOCAL_STORAGE_KEY = "videoplayer-current-time";
-const timeFromLocalStorage = localStorage.getItem(LOCAL_STORAGE_KEY) || 0;
+const timeFromLocalStorage = localStorage.getItem(LOCAL_STORAGE_KEY);
+
+const currentTimeFromLocalStorage = timeFromLocalStorage ? parseFloat(timeFromLocalStorage) : 0;
 
 const onPlay = function(data) {
     const currentTimeWatchingVideo = data.seconds;
@@ -15,8 +15,13 @@ const onPlay = function(data) {
 
 player.on('timeupdate', throttle(onPlay, 1000));
 
-player.setCurrentTime(timeFromLocalStorage);
+player.setCurrentTime(currentTimeFromLocalStorage);
 
 player.getVideoTitle().then(function(title) {
     console.log('title:', title);
 });
+
+
+
+
+
